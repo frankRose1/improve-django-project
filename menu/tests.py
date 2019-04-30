61893d2eaf21fafeee84356c11c01085d466aca8
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-04-29 23:58:18 -0400

    Add debug_toolbar

--- menu/tests.py
+++ menu/tests.py
@@ -1,17 +1,44 @@
 from datetime import datetime
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.utils import timezone
-from .models import Menu, Item
+from .models import Menu, Item, Ingredient
 
 
-class MenuModelTest(TestCase):
+class MenuModelTests(TestCase):
     def test_menu_creation(self):
         menu = Menu.objects.create(
           season='summer',
         )
         self.assertEqual(menu.season, 'summer')
         self.assertLessEqual(menu.created_date, timezone.now())
+
+
+class ItemModelTests(TestCase):
+    def test_item_creation(self):
+        chef = User.objects.create_user(
+            username='top_chef45',
+            email='top.chef45@gmail.com',
+            password='top_secret_pw'         
+        )
+        item = Item.objects.create(
+            name='Orange Juice',
+            standard=False,
+            chef=chef   
+        )
+        self.assertEqual(item.name, 'Orange Juice')
+        self.assertEqual(chef.id, item.chef_id)
+        self.assertLessEqual(item.created_date, timezone.now())
+
+
+class IngredientModelTests(TestCase):
+    def test_ingredient_creation(self):
+        ingredient = Ingredient.objects.create(
+            name='sugar'
+        )
+        self.assertEqual(ingredient.name, 'sugar')
+        self.assertEqual(ingredient.id, 1)
 
 
 class MenuViewsTests(TestCase):
@@ -38,14 +65,21 @@ class MenuViewsTests(TestCase):
         res = self.client.get(reverse('menu:detail', kwargs={'pk': self.menu.id}))
         self.assertEqual(res.status_code, 200)
         self.assertEqual(self.menu, res.context['menu'])
+        self.assertTemplateUsed(res, 'menu/menu_detail.html')
 
 
 class ItemViewsTests(TestCase):
 
     def setUp(self):
+        self.chef = User.objects.create_user(
+          username='testUser123',
+          email='test.user@gmail.com',
+          password='testPassword567!'
+        )
         self.item = Item.objects.create(
             name='New Menu Item',
-            standard=True
+            standard=True,
+            chef=self.chef
         )
 
     def test_item_detail(self):
